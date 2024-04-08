a0c85aed4021a95f69503a917c10cc20a028ba24
srikar <srikaraleti95@gmail.com>
2024-04-08 11:02:56 +0530

    first commit

---
+++ src/main/java/com/jwt/sri/models/AuthenticationResponse.java
@@ -0,0 +1,17 @@
+package com.jwt.sri.models;
+
+import java.io.Serializable;
+
+public class AuthenticationResponse implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+	private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
---
+++ src/main/java/com/jwt/sri/security/MyUserDetailsService.java
@@ -0,0 +1,18 @@
+package com.jwt.sri.security;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+		return new User("srikar", "srikar@123", new ArrayList<>());
+	}
+}
